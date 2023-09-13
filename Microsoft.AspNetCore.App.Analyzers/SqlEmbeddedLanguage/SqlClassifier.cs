4103584ddc67e7ff18a7701c84ad9e6b3ff99429
Brice Lambson <brice@bricelam.net>
2023-09-13 15:16:22 -0700

    Add some code

---
+++ Microsoft.AspNetCore.App.Analyzers/SqlEmbeddedLanguage/SqlClassifier.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Classification;
+using Microsoft.CodeAnalysis.ExternalAccess.AspNetCore.EmbeddedLanguages;
+
+namespace Microsoft.AspNetCore.Analyzers.SqlEmbeddedLanguage;
+
+[ExportAspNetCoreEmbeddedLanguageClassifier("Sql", LanguageNames.CSharp)]
+internal class SqlClassifier : IAspNetCoreEmbeddedLanguageClassifier
+{
+    public void RegisterClassifications(AspNetCoreEmbeddedLanguageClassificationContext context)
+    {
+        // TODO
+        context.AddClassification(ClassificationTypeNames.RegexAnchor, context.SyntaxToken.Span);
+    }
+}
